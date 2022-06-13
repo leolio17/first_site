4ac58bab7076dc50faeed3b49478c6da3c2949a4
youngcheol.lim <yc84.lim@gmail.com>
2022-06-13 15:52:20 +0900

    [lyc] 2-04 complete
    [desc] 2-04까지 완료

--- first_site/views.py
+++ first_site/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 #edited
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from .models import Question
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("===첫 페이지 ===")
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'first_site/question_list.html', context)
+    #return HttpResponse("===첫 페이지 ===")
+
+
+def detail(request, question_id):
+    #question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'first_site/question_detail.html', context)
